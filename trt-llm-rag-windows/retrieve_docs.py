a905b4f904363bb5cbe71a8f68f1a0810412c1d6
Bryan Etzine <bryetz@gmail.com>
2024-02-23 19:29:58 -0500

    Update and rename retrieve.py to retrieve_docs.py

--- trt-llm-rag-windows/retrieve_docs.py
+++ trt-llm-rag-windows/retrieve_docs.py
@@ -11,7 +11,7 @@ def retrieve(query):
         string: Context retrieved based on the query.
     """
     # Endpoint URL of the Flask server
-    url = "http://127.0.0.1:5000/rag"
+    url = "http://127.0.0.1:5000/retrieve_docs"
     
     # Prepare the request payload
     payload = {"query": query}
@@ -20,7 +20,7 @@ def retrieve(query):
     try:
         response = requests.post(url, json=payload)
         data = response.json()  # Parse the JSON response
-        return data["document"]
+        return data["documents"]
     
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while retrieving documents: {e}")
